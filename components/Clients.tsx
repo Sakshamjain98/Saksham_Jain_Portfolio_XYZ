d1845cce503e5fb4476f97ec90ff37a07fcb1e0a
Saksham Jain <sakshambro730@gmail.com>
2025-03-15 22:54:39 +0530

    Update-5

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -9,24 +9,6 @@ import { LampContainer } from "./ui/lamp";
 const Clients = () => {
   return (
     <section id="testimonials" className="">
-      <h1 className="heading mb-10">
-        Kind words from
-        <span className="text-purple"> satisfied clients</span>
-      </h1>
-      <LampContainer>
-        <motion.h1
-          initial={{ opacity: 0.5, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="mt-16 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-        >
-          Coming <br /> Soon
-        </motion.h1>
-      </LampContainer>
       <div className="flex flex-col items-center ">
         {/* <div
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
@@ -39,7 +21,7 @@ const Clients = () => {
           />
         </div> */}
 
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-0">
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 ">
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
